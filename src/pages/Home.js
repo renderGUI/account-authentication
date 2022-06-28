bfd24988c4f72e649d436c5537c3e0a0a36e3b4d
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-06-28 10:50:11 -0400

    Create chat UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 import NavigationHeader from "../components/NavigationHeader";
 import classes from "./Home.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <div className={classes.container}>
       <NavigationHeader />
-      <h2>Homepage</h2>
+      <div className={classes.chatContainer}>
+        <div className={classes.chatBox}>
+          <form>
+            <input type="text"></input>
+            <button className={classes.sendButton} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
